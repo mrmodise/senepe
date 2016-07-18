5fff6a061552b7550a5f3376dfe1d567b2f001e5
mrmodise <modisemorebodi@gmail.com>
2016-07-18 13:30:08 +0200

    added JQuery setup to typings

---
+++ typings/index.d.ts
@@ -0,0 +1,8 @@
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+interface JQuery {
+    chosen(options?:any):JQuery;
+    particleground(options?:any):JQuery;
+}  
