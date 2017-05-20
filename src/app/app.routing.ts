355241bb2b0d6eb9c24cac7f80890b89b968cc29
mrmodise <modisemorebodi@gmail.com>
2017-05-20 19:10:34 +0200

    added register and fallback routes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import {ModuleWithProviders} from "@angular/core";
 // custom
 import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
+import {RegisterComponent} from "./components/register/register.component";
 
 // define routes
 const routes: Routes = [
@@ -20,6 +21,14 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: '**',
+    component: HomeComponent
   }
 ];
 
