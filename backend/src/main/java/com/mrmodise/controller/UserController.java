014befb1f304c51a8e205e6492c697877b10def2
mrmodise <modisemorebodi@gmail.com>
2017-05-20 22:43:53 +0200

    updated server login logic

--- backend/src/main/java/com/mrmodise/controller/UserController.java
+++ backend/src/main/java/com/mrmodise/controller/UserController.java
@@ -5,7 +5,9 @@ import java.util.Map;
 
 import javax.servlet.ServletException;
 
+import com.sun.net.httpserver.Authenticator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -30,32 +32,51 @@ public class UserController {
 		
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(@RequestBody Map<String, String> json) throws ServletException {
+	@RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json")
+	public ResponseEntity<String> login(@RequestBody Map<String, String> json) throws ServletException {
+
+		// set content-type
+		final HttpHeaders httpHeaders= new HttpHeaders();
+		httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+		// error messages placeholder
+		String message;
+
+		// if both username and password do not exist
 		if(json.get("username") == null || json.get("password") == null){
-			throw new ServletException("Please fill in username and password");
+			message = "{\"message\": \"Please fill in username and password\"}";
+			// write response to client
+			return new ResponseEntity<>(message, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+		// retrieve user credentials
 		String userName = json.get("username");
 		String password = json.get("password");
-		
+
+		// retrieve single user credentials
 		User user = userService.findByUsername(userName);
-		
+
+		// no user found
 		if (user == null){
-			throw new ServletException("User not found");
+			message = "{\"message\": \"User not found\"}";
+			// write response to client
+			return new ResponseEntity<>(message, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
 		String pwd = user.getPassword();
-		
+
+		// password mismatch
 		if(!password.equals(pwd)){
-			throw new ServletException("Invalid login. Please check your username and password");
+			message = "{\"message\": \"Invalid login. Please check your username and password\"}";
+			// write response to client - error 500
+			return new ResponseEntity<>(message, httpHeaders, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		return Jwts.builder()
+		// token builder
+		String res = Jwts.builder()
 				.setSubject(userName)
 				.claim("roles", "user")
 				.setIssuedAt(new Date())
 				.signWith(SignatureAlgorithm.HS256, "montsamaisabosigo")
 				.compact();
+		// return OK response
+		return new ResponseEntity<>(res, HttpStatus.OK);
 	}
 }
