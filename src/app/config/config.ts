b3ae27c151ce62965dc750a8ac1486f60a84beaa
mrmodise <modisemorebodi@gmail.com>
2017-05-27 23:35:12 +0200

    changed server ports

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -10,7 +10,7 @@ export class Config{
   public POST_USER_URL = 'http://localhost:8088/user/user';
   public PHOTO_BY_ID_URL = 'http://localhost:8088/photo/get';
   public UPDATE_PHOTO_URL = 'http://localhost:8088/photo/update';
-  public LOGIN_URL = 'http://localhost:8088/user/login';
+  public LOGIN_URL = 'http://localhost:8088/auth';
   public TOKENIZE_URL = 'http://localhost:8011/rest/user/users';
 
   // headers
