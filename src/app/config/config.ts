1d474d85f2fb54ef1101264c581d94933271aac2
mrmodise <modisemorebodi@gmail.com>
2017-06-03 00:55:58 +0200

    added more global configurations

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -12,10 +12,11 @@ export class Config {
   public UPDATE_PHOTO_URL = 'http://localhost:8088/photo/update';
   public LOGIN_URL = 'http://localhost:8088/auth';
   public TOKENIZE_URL = 'http://localhost:8088/rest/user/users';
-  public USER_BY_NAME_URL = 'http://localhost:8088/rest/user';
-  public ADD_PHOTO_URL = 'http://localhost:8088/photo/add';
+  public USER_BY_NAME_URL = 'http://localhost:8088/rest/photo/user';
+  public ADD_PHOTO_URL = 'http://localhost:8088/rest/photo/add';
+  public FILE_REQUEST_URL = 'http://localhost:8088/rest/photo/upload';
 
   // headers
-  public JSON_HEADERS: Headers = new Headers({'Content-Type': 'application/json'});
+  public JSON_HEADERS = new Headers({'Content-Type': 'application/json'});
 
 }
