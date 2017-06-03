a353672c25483acc2c1bdfdb3821596e0d046a28
mrmodise <modisemorebodi@gmail.com>
2017-06-03 14:50:08 +0200

    provided the HttpClient service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 // defaults
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 // custom components
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { PhotoListComponent } from './components/photo-list/photo-list.component';
-import { SidePanelComponent } from './components/side-panel/side-panel.component';
-import { LoginComponent } from './components/login/login.component';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {RegisterComponent} from './components/register/register.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {PhotoListComponent} from './components/photo-list/photo-list.component';
+import {SidePanelComponent} from './components/side-panel/side-panel.component';
+import {LoginComponent} from './components/login/login.component';
 
 // services
 import {RegisterService} from './services/register.service';
@@ -20,13 +20,14 @@ import {LoginService} from './services/login.service';
 
 // routing
 import {routing} from './app.routing';
-import { NotFoundComponent } from './components/not-found/not-found.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
 import {AuthService} from './services/auth.service';
-import { AlbumComponent } from './components/album/album.component';
+import {AlbumComponent} from './components/album/album.component';
 import {UserService} from './services/user.service';
-import { AddPhotoComponent } from './components/add-photo/add-photo.component';
+import {AddPhotoComponent} from './components/add-photo/add-photo.component';
 import {AddPhotoService} from './services/add-photo.service';
 import {UploadPhotoService} from './services/upload-photo.service';
+import {HttpClientService} from "./services/http-client.service";
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import {UploadPhotoService} from './services/upload-photo.service';
     HttpModule,
     routing
   ],
-  providers: [RegisterService, PhotoService, LoginService, AuthService, UserService, AddPhotoService, UploadPhotoService],
+  providers: [RegisterService, PhotoService, LoginService, HttpClientService, AuthService, UserService, AddPhotoService, UploadPhotoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
