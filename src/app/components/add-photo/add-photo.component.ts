4d2088207d4d952b2bcf9b86af747555112d2c12
mrmodise <modisemorebodi@gmail.com>
2017-07-08 18:11:49 +0200

    removed unused imports

--- src/app/components/add-photo/add-photo.component.ts
+++ src/app/components/add-photo/add-photo.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import {UserService} from '../../services/user.service';
-import {User} from '../../models/user';
 import {Photo} from '../../models/photo';
 import {AddPhotoService} from "../../services/add-photo.service";
 import {UploadPhotoService} from "../../services/upload-photo.service";
