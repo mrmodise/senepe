48681bf297cbf1a4649122b2231180c3599b7c6c
mrmodise <modisemorebodi@gmail.com>
2017-08-10 21:34:37 +0200

    converting forms to reactive forms

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 import {RegisterService} from '../../services/register.service';
 import {User} from '../../models/user';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,28 +13,37 @@ export class RegisterComponent implements OnInit {
 
   user = new User();
   registered = false;
-  message;
+  message: string;
   error = false;
+  registerForm: FormGroup;
 
-  constructor(private http: Http, private registerService: RegisterService) {
+  constructor(private http: Http,
+              private fb: FormBuilder,
+              private registerService: RegisterService) {
   }
 
   ngOnInit() {
+    this.createForm();
   }
 
-  onSubmit() {
-
-    console.log('Data ' + JSON.stringify(this.user));
+  onSubmit(): void {
 
     this.registerService.register(this.user).subscribe(data => {
-      console.log(data);
       this.registered = true;
       this.message = data.message;
 
     }, error => {
-      console.log(error);
       this.error = true;
       this.message = error.message;
+    });
+  }
+
+  private createForm(): void{
+    this.registerForm = this.fb.group({
+        username: ['', Validators.required],
+        fullName: ['', Validators.required],
+        email: ['', Validators.required],
+        password: ['', Validators.required]
     });
   }
 
