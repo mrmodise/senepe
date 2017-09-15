a47502d5b181b1987b66b9f7edb547b654579bc3
mrmodise <modisemorebodi@gmail.com>
2017-09-15 23:18:20 +0200

    updated register form validations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -11,10 +11,13 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   user: User;
+  registerForm: FormGroup;
   registered = false;
   message: string;
   error = false;
-  registerForm: FormGroup;
+  isUserNameValid = true;
+  isEmailValid = true;
+  isPasswordValid = true;
 
   constructor(private fb: FormBuilder,
               private registerService: RegisterService) {
@@ -24,7 +27,19 @@ export class RegisterComponent implements OnInit {
     this.createForm();
   }
 
+  /**
+   * submits form to the server
+   */
   onSubmit(): void {
+
+    // additional safety checks
+    if(!this.validateEmail()) {
+      return;
+    } else if (!this.validatePassword()){
+      return;
+    }else if (!this.validateUserName()){
+      return;
+    }
 
     this.registerService.register(this.registerForm.value).subscribe(data => {
       this.registered = true;
@@ -32,17 +47,53 @@ export class RegisterComponent implements OnInit {
 
     }, error => {
       this.error = true;
-      this.message = error.message;
+      this.message = error.message || 'Something went wrong';
     });
   }
 
-  private createForm(): void{
+  /**
+   * instantiates form model
+   */
+  private createForm(): void {
     this.registerForm = this.fb.group({
-        username: ['', [Validators.required, Validators.minLength(6)]],
-        fullname: ['', [Validators.required, Validators.minLength(6)]],
-        email: ['', [Validators.required, Validators.pattern("[^@]*@[^@]*")]],
-        password: ['', [Validators.required, Validators.minLength(8)]]
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      fullName: [''],
+      email: ['', [Validators.required, Validators.pattern('[^@]*@[^@]*')]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
     });
+  }
+
+  /**
+   * validates username length
+   */
+  validateUserName() {
+    if (this.registerForm.controls['username'].valid) {
+      return this.isUserNameValid = true;
+    } else {
+      return this.isUserNameValid = false;
+    }
+  }
+
+  /**
+   * validates email format
+   */
+  validateEmail() {
+    if (this.registerForm.controls['email'].valid) {
+      return this.isEmailValid = true;
+    } else {
+      return this.isEmailValid = false;
+    }
+  }
+
+  /**
+   * validates password length
+   */
+  validatePassword() {
+    if (this.registerForm.controls['password'].valid) {
+      return this.isPasswordValid = true;
+    } else {
+      return this.isPasswordValid = false;
+    }
   }
 
 }
