74538543aae9b060206c113afe2f926f6c96e60b
mrmodise <modisemorebodi@gmail.com>
2017-09-17 08:23:28 +0200

    front-end: add-photo component tests clean-up

--- src/app/components/add-photo/add-photo.component.spec.ts
+++ src/app/components/add-photo/add-photo.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {AddPhotoComponent} from './add-photo.component';
 import {UserService} from '../../services/user.service';
 import {AddPhotoService} from '../../services/add-photo.service';
@@ -23,34 +23,75 @@ describe('AddPhotoComponent', () => {
   }));
 
   beforeEach(() => {
+    // create component and test fixture
     fixture = TestBed.createComponent(AddPhotoComponent);
+    // get test component from the fixture
     component = fixture.componentInstance;
+    // trigger lifecycle function to create form
+    component.ngOnInit();
+    // watch changes in the fixture
     fixture.detectChanges();
   });
 
+  // COMPONENT tests
   it('should create AddPhotoComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it(`should render title in h2 tag`, () => {
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h2').textContent).toEqual('Uploading your favorite photos has never been this easy');
-  });
+  // FORM tests
+  it('should create the add-photo form', (() => {
+    expect(component.createForm()).toBeTruthy();
+  }));
 
-  it('should have default properties', fakeAsync(() => {
+  it('add-photo form should have default empty properties', (() => {
     expect(component.addPhotoForm.value).toEqual(blankForm);
   }));
 
-  it('should initialize form fields', fakeAsync(() => {
+  it('should initialize add-photo form fields', (() => {
     updateFormAttributes('Rekz', 'Rekz photo', 'This is Rekz photo');
     expect(component.addPhotoForm.value).toEqual(populatedForm);
   }));
 
-  it('photoAdded should be false if error occurred', async(() => {
+  it('photoAdded should be false if error occurred', (() => {
     updateFormAttributes(blankForm.photoName, blankForm.title, blankForm.description);
     component.onSubmit();
     expect(component.photoAdded).toBeFalsy();
+  }));
+
+  // PHOTO NAME field tests
+  it('photo name should be invalid', (() => {
+    const photoName = component.addPhotoForm.controls['photoName'];
+    expect(photoName.valid).toBeFalsy();
+  }));
+
+  it('isPhotoNameValid should be false if photo name is empty', (() => {
+    expect(component.validatePhotoName()).toBeFalsy();
+  }));
+
+  it('isPhotoNameValid should be true if photo name is provided', (() => {
+    setPhotoName('Praise photo');
+    expect(component.validatePhotoName()).toBeTruthy();
+  }));
+
+  // PHOTO TITLE field tests
+  it('photo title should be invalid', (() => {
+    const photoTitle = component.addPhotoForm.controls['title'];
+    expect(photoTitle.valid).toBeFalsy();
+  }));
+
+  it('isPhotoTitleValid should be false if photo name is empty', (() => {
+    expect(component.validatePhotoTitle()).toBeFalsy();
+  }));
+
+  it('isPhotoTitleValid should be true if photo name is provided', (() => {
+    setPhotoTitle('Praise photo album');
+    expect(component.validatePhotoTitle()).toBeTruthy();
+  }));
+
+  // PHOTO DESCRIPTION field tests
+  it('photo description should be valid', (() => {
+    const photoDescription = component.addPhotoForm.controls['description'];
+    expect(photoDescription.valid).toBeTruthy();
   }));
 
   /**
@@ -60,6 +101,22 @@ describe('AddPhotoComponent', () => {
     component.addPhotoForm.controls['photoName'].setValue(photoName);
     component.addPhotoForm.controls['title'].setValue(title);
     component.addPhotoForm.controls['description'].setValue(description);
+  }
+
+  /**
+   * reusable function for photo name dry spec
+   * @param photoName
+   */
+  function setPhotoName(photoName) {
+    component.addPhotoForm.controls['photoName'].setValue(photoName);
+  }
+
+  /**
+   * reusable function for photo name dry spec
+   * @param photoTitle
+   */
+  function setPhotoTitle(photoTitle) {
+    component.addPhotoForm.controls['title'].setValue(photoTitle);
   }
 
 });
