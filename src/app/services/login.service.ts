b3d0ae3a05e5c0ac89fdfd058855e02f8320d297
Morebodi Modise <morebodi@codeforte.com>
2017-09-18 16:05:20 +0200

    refactored login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,49 +1,30 @@
 // defaults
 import {Injectable} from '@angular/core';
-
 // RxJS
 import {Observable} from 'rxjs/Observable';
-
 // custom
 import {User} from '../models/user';
 import {Config} from '../config/config';
-
 // router
 import {Router} from '@angular/router';
-import {HttpClientService} from './http-client.service';
+import {Headers, Http, RequestOptions} from '@angular/http';
 
 @Injectable()
 export class LoginService {
 
-  /**
-   * Checks if user is authenticated
-   * @returns {boolean}
-   */
-  static isAuthenticated(): boolean {
-    const userName = localStorage.getItem('currentUserName');
-    const token = localStorage.getItem('token');
-
-    if ((userName !== '' && userName !== null) && (token !== '' && token !== null)) {
-      // if the user and token exists in the local storage, user is authenticated
-      return true;
-    } else {
-      return false;
-    }
+  constructor(private http: Http, private router: Router) {
   }
-
-  constructor(private httpClient: HttpClientService, private router: Router) {
-  }
-
 
   /**
    * Pings the server for a login request
    * @param model
    * @returns {Observable<User>}
    */
-  login(model): Observable<User> {
+  login(model): Observable<any> {
     return this
-      .httpClient
-      .post(Config.LOGIN_URL, model, Config.JSON_HEADERS);
+      .http
+      .post(Config.LOGIN_URL, JSON.stringify(model), this.getOptions())
+      .map(res => res.json());
   }
 
   /**
@@ -55,5 +36,33 @@ export class LoginService {
     localStorage.setItem('currentUserName', '');
     this.router.navigate(['home']);
     // alert('You have been logged out');
+  }
+
+  /**
+   * Checks if user is authenticated
+   * @returns {boolean}
+   */
+  isAuthenticated(): boolean {
+    const userName = localStorage.getItem('currentUserName');
+    const token = localStorage.getItem('token');
+
+    if ((userName !== '' && userName !== null) && (token !== '' && token !== null)) {
+      // if the user and token exists in the local storage, user is authenticated
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * retrieve headers
+   * @returns {RequestOptions}
+   */
+  private getOptions(): RequestOptions {
+    const headers: Headers = new Headers();
+    headers.append('content-type', 'application/json; charset=utf-8');
+    const opts = new RequestOptions({headers: headers});
+    opts.headers = headers;
+    return opts;
   }
 }
