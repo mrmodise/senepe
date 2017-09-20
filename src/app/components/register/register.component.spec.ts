94ff7367be0f5e9add9a41ae9ec04f72d43cc6e5
Morebodi Modise <morebodi@codeforte.com>
2017-09-20 10:59:50 +0200

    added more register component tests

--- src/app/components/register/register.component.spec.ts
+++ src/app/components/register/register.component.spec.ts
@@ -6,12 +6,13 @@ import {RegisterComponent} from './register.component';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
-  let populateRegister = {
+  const populateRegister = {
     username: 'tester1',
     fullname: 'tester1 tester1',
     email: 'tester1@gmail.com',
     password: 'tester123'
   };
+  let username, fullname, email, password;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -29,6 +30,12 @@ describe('RegisterComponent', () => {
     component.ngOnInit();
     // watch changes in the fixture
     fixture.detectChanges();
+
+    username = component.registerForm.controls['username'];
+    fullname = component.registerForm.controls['fullname'];
+    email = component.registerForm.controls['email'];
+    password = component.registerForm.controls['password'];
+
   });
 
   // COMPONENT tests
@@ -37,15 +44,15 @@ describe('RegisterComponent', () => {
   });
 
   // GENERAL FORM tests
-  it('should create login form', (() => {
+  it('should create registration form', (() => {
     expect(component.createForm()).toBeTruthy();
   }));
 
-  it('should create invalid register form', (() => {
+  it('should create invalid registration form', (() => {
     expect(component.registerForm.valid).toBeFalsy();
   }));
 
-  it('should initialize register form fields', (() => {
+  it('should initialize registration form fields', (() => {
     populateForm(populateRegister.username,
       populateRegister.fullname,
       populateRegister.email,
@@ -53,19 +60,76 @@ describe('RegisterComponent', () => {
     expect(component.registerForm.value).toEqual(populateRegister);
   }));
 
+  // USERNAME field tests
+  it('username should be required', (() => {
+    const errors = username.errors || {};
+    expect(errors['required']).toBeTruthy();
+  }));
+
+  it('username should be more than 6 characters', (() => {
+    component.registerForm.controls['username'].setValue('test');
+    const errors = username.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+  }));
+
+  it('username should be valid', (() => {
+    component.registerForm.controls['username'].setValue('tester1');
+    expect(component.validateUserName()).toBeTruthy();
+  }));
+
+  // PASSWORD field tests
+  it('password should be required', (() => {
+    const errors = password.errors || {};
+    expect(errors['required']).toBeTruthy();
+  }));
+
+  it('password should be more than 8 characters', (() => {
+    component.registerForm.controls['password'].setValue('tester1');
+    const errors = password.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+  }));
+
+  it('password should be valid', (() => {
+    component.registerForm.controls['password'].setValue('tester123');
+    expect(component.validatePassword()).toBeTruthy();
+  }));
+
+  // FULLNAME field tests
+  it('full name should be valid', (() => {
+    expect(fullname.invalid).toBeTruthy();
+  }));
+
+  // EMAIL field tests
+  it('email should be required', (() => {
+    const errors = email.errors || {};
+    expect(errors['required']).toBeTruthy();
+  }));
+
+  it('email should be in correct format', (() => {
+    component.registerForm.controls['email'].setValue('tester1');
+    const errors = email.errors || {};
+    expect(errors['pattern']).toBeTruthy();
+  }));
+
+  it('email should be valid', (() => {
+    component.registerForm.controls['email'].setValue('tester1@gmail.com');
+    expect(component.validateEmail()).toBeTruthy();
+  }));
+
+
   /**
    * reusable function for a dry spec.
-   * @param {string} username
+   * @param {string} userName
    * @param {string} fullName
-   * @param {string} email
-   * @param {string} password
+   * @param {string} userEmail
+   * @param {string} userPassword
    */
-  function populateForm(username: string, fullName: string,
-                        email: string, password: string) {
-    component.registerForm.controls['username'].setValue(username);
+  function populateForm(userName: string, fullName: string,
+                        userEmail: string, userPassword: string) {
+    component.registerForm.controls['username'].setValue(userName);
     component.registerForm.controls['fullname'].setValue(fullName);
-    component.registerForm.controls['email'].setValue(email);
-    component.registerForm.controls['password'].setValue(password);
+    component.registerForm.controls['email'].setValue(userEmail);
+    component.registerForm.controls['password'].setValue(userPassword);
   }
 
 });
