4d084ee206ab8d094ba8f90219b60550243c6f15
pnyama <modisemorebodi@gmail.com>
2017-10-05 16:42:17 +0200

    adding redux dev tools

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,7 +1,11 @@
-import {createStore} from 'redux';
+import {compose, createStore, GenericStoreEnhancer} from 'redux';
 import {IAppState} from './IAppState';
 import {reducer} from './reducers';
 
-export const store = createStore<IAppState>(reducer);
+declare var window: any;
+const devToolsExtension: GenericStoreEnhancer = (window.devToolsExtension)
+  ? window.devToolsExtension() : (f) => f;
+
+export const store = createStore<IAppState>(reducer, compose(devToolsExtension) as GenericStoreEnhancer);
 
 
