e6c34f31992dc30a7f20d74f8f0d137c8b982196
pnyama <modisemorebodi@gmail.com>
2017-10-08 14:46:06 +0200

    chore: models clean-up

--- src/app/models/photo.ts
+++ src/app/models/photo.ts
@@ -1,6 +1,18 @@
-export class Photo {
+export interface IPhoto {
+  photoId: string;
+  likes: number;
+  title: string;
+  imageName: string;
+  description: string;
+}
+
+export class Photo implements IPhoto {
   public photoId: string;
   public likes: number;
   public title: string;
   public imageName: string;
+  public description: string;
+
+  constructor() {
+  }
 }
