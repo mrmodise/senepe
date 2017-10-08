e9e9f421a6eab9a52f17a425ea3b678e92c96458
pnyama <modisemorebodi@gmail.com>
2017-10-08 14:43:28 +0200

    chore: code clean-up

--- src/app/components/album/album.component.ts
+++ src/app/components/album/album.component.ts
@@ -1,11 +1,9 @@
 // defaults
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
-
 // custom
 import {Photo} from '../../models/photo';
-import {PhotoService} from '../../services/photo.service';
-import {UserService} from '../../services/user.service';
+import {User} from '../../models/user';
 
 @Component({
   selector: 'app-album',
@@ -15,16 +13,12 @@ import {UserService} from '../../services/user.service';
 
 export class AlbumComponent implements OnInit {
 
-  // variables initialization
-  photos: Photo[];
-  user;
+  photos: Array<Photo>;
+  user: User;
   selectedPhoto: Photo;
   parentRouter: Router;
-  currentUser = localStorage.getItem('currentUserName');
 
-  constructor(private photoService: PhotoService,
-              private router: Router,
-              private userService: UserService) {
+  constructor() {
   }
 
   ngOnInit() {
@@ -32,7 +26,7 @@ export class AlbumComponent implements OnInit {
 
   onSelect(photo: Photo) {
     this.selectedPhoto = photo;
-    this.parentRouter.navigate(['/image-details', this.selectedPhoto.photoId]);
+    this.parentRouter.navigate(['/image-detail', this.selectedPhoto.photoId]);
   }
 
 }
