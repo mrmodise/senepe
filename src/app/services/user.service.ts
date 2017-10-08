45ca4bd6ede449e7bf907580f53f44db4fa6ce96
pnyama <modisemorebodi@gmail.com>
2017-10-08 08:45:20 +0200

    chore: updated user service to retrieve current user profile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -8,17 +8,29 @@ import {HttpClientService} from './http-client.service';
 @Injectable()
 export class UserService {
 
-  constructor(private httpClient: HttpClientService) {}
+  constructor(private httpClient: HttpClientService) {
+  }
 
   /**
    *
    * @param {User} user
    * @returns {Observable<Photo[]>}
    */
-  public getPhotosByUser(user: User): Observable<Photo[]> {
+  getPhotosByUser(user: User): Observable<Photo[]> {
     return this
       .httpClient
       .post(Config.GET_USER_PHOTOS, user, Config.AUTH_HEADERS);
+  }
+
+  /**
+   * retrieves currently logged in user's photo details
+   * @param {string} userName
+   * @returns {User}
+   */
+  getCurrentUser(userName: string): Observable<User> {
+    return this
+      .httpClient
+      .post(Config.POST_USER_URL, userName, Config.AUTH_HEADERS);
   }
 
 }
